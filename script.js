d5a9aa1f3159bdfc6dc1350305a5f1d0e09ab62d
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2025-08-09 12:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function increment(){
+	const counterElement = document.getElementById("counter");
+      const currentValue = parseInt(counterElement.textContent);
+      alert(currentValue); // Show un-incremented value
+      counterElement.textContent = currentValue + 1;
+}
